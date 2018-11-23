ee0659c4d31cdd7c315f8c854339881d60d4804d
muruganantham-l <41933573+muruganantham-l@users.noreply.github.com>
2018-11-23 12:05:59 +0800

    Today_scorecard_all ast_det table added
    Today_scorecard_all ast_det table added

--- SQL/Today_scorecard_all.sql
+++ SQL/Today_scorecard_all.sql
@@ -7,15 +7,20 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and wko_mst_asset_level = SatateDesc
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by not in ('Patch')
+--and ast_mst_create_by not in ('Patch')
+
+and ast_det_varchar22  in ('PUR-EX', 'NEW-BE' ,'EXISTING','NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and convert(date,wkr_mst_org_date) = convert(date,getdate())
 group by Statecode , Year(wkr_mst_org_date) ,Convert(date,wkr_mst_org_date) 
 --order by Year(wkr_mst_org_date) , Datename(MONTH,wkr_mst_org_date) 
@@ -54,14 +59,19 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by not in ('Patch')
+--and ast_mst_create_by not in ('Patch')
+
+and ast_det_varchar22  in ('PUR-EX', 'NEW-BE' ,'EXISTING','NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and wko_mst_asset_level = SatateDesc
 and convert(date,wkr_mst_org_date) = convert(date,getdate())
 and wko_mst_status in ('OPE','RFS')
@@ -76,7 +86,9 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
+and ast_mst.RowID = ast_det.mst_RowID
 and wko_mst.RowID = wko_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
@@ -84,7 +96,10 @@ and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and wko_mst_asset_level = SatateDesc
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by in ('Patch')
+--and ast_mst_create_by in ('Patch')
+
+and ast_det_varchar22  in ('NEW', 'PUR' ,'NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and convert(date,wkr_mst_org_date) = convert(date,getdate())
 group by Statecode , Year(wkr_mst_org_date) ,Convert(date,wkr_mst_org_date) 
 --order by Year(wkr_mst_org_date) , Datename(MONTH,wkr_mst_org_date) 
@@ -123,15 +138,20 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by in ('Patch')
+--and ast_mst_create_by in ('Patch')
 and wko_mst_asset_level = SatateDesc
+
+and ast_det_varchar22  in ('NEW', 'PUR' ,'NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and convert(date,wkr_mst_org_date) = convert(date,getdate())
 and wko_mst_status in ('OPE','RFS')
 group by Statecode , Year(wkr_mst_org_date) ,Convert(date,wkr_mst_org_date) 
