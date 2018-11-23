b917b46bf859456f5ed9bf130b84b01f720da221
muruganantham-l <41933573+muruganantham-l@users.noreply.github.com>
2018-11-23 12:02:12 +0800

    Zone_Scorecard_view_All ast_det table added
    Zone_Scorecard_view_All ast_det table added

--- SQL/Zone_Scorecard_view_All.sql
+++ SQL/Zone_Scorecard_view_All.sql
@@ -6,14 +6,18 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by not in ('Patch')
+--and ast_mst_create_by not in ('Patch')
+and ast_det_varchar22  in ('PUR-EX', 'NEW-BE' ,'EXISTING','NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and Year(wkr_mst_org_date) >= year(getdate())-1
 group by ast_mst_perm_id 
 
@@ -47,14 +51,18 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by not in ('Patch')
+--and ast_mst_create_by not in ('Patch')
+and ast_det_varchar22  in ('PUR-EX', 'NEW-BE' ,'EXISTING','NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and Year(wkr_mst_org_date) >= year(getdate())-1
 and wko_mst_status in ('OPE','RFS')
 group by ast_mst_perm_id 
@@ -67,14 +75,19 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by in ('Patch')
+--and ast_mst_create_by in ('Patch')
+
+and ast_det_varchar22 in ('NEW', 'PUR' ,'NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and Year(wkr_mst_org_date) >= year(getdate())-1
 group by ast_mst_perm_id 
 
@@ -108,14 +121,19 @@ from wko_mst (nolock)
 ,wko_det (nolock)
 ,wkr_mst (nolock)
 ,ast_mst (nolock)
+,ast_det (NOLOCK)
 where wko_mst.site_cd = wko_det.site_cd
 and wko_mst.RowID = wko_det.mst_RowID
+and ast_mst.RowID = ast_det.mst_RowID
 and left(wko_mst_wo_no,3) = 'CWO'
 and wkr_mst.site_cd = wko_det.site_cd
 and wko_det_wr_no = wkr_mst.wkr_mst_wr_no
 and ast_mst.site_cd    = wko_mst.site_cd
 and ast_mst_asset_no = wko_mst_assetno
-and ast_mst_create_by in ('Patch')
+--and ast_mst_create_by in ('Patch')
+
+and ast_det_varchar22 in ('NEW', 'PUR' ,'NA')
+and isnull(ast_mst_parent_id ,ast_mst_asset_no)= ast_mst_asset_no
 and Year(wkr_mst_org_date) >= year(getdate())-1
 and wko_mst_status in ('OPE','RFS')
 group by ast_mst_perm_id 
