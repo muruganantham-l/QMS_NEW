8498eb5e4a6cc088fbbcb54971d5f68e9c0f5cdc
muruganantham-l <41933573+muruganantham-l@users.noreply.github.com>
2018-12-07 18:40:12 +0800

    kpi graph yearlywise development
    kpi graph yearlywise development

--- SQL/BATCH/wo/wo_date_update.sql
+++ SQL/BATCH/wo/wo_date_update.sql
@@ -1,10 +1,43 @@
 ALTER proc wo_date_update
-@wo_no varchar(100) = 'CWO191152'
-,@cmpl_date datetime = '2018-11-19 16:29:00.000'
-,@response_date datetime = NULL
+@wo_no varchar(100) = 'cwo190486'
+,@cmpl_date datetime = '2018-11-09 11:20:00.000'
+,@response_date datetime = null
 as
 begin
-set nocount ON
+ 
+
+declare @wr_date datetime
+,@wo_ack_date datetime
+
+select @wr_date = w.wkr_mst_org_date,@cmpl_date = isnull(@cmpl_date, d.wko_det_cmpl_date),@response_date = isnull(@response_date,d.wko_det_exc_date),@wo_ack_date = d.wko_det_sched_date
+from wko_mst m (nolock) join wko_det d (NOLOCK)
+on m.RowID = d.mst_RowID
+and m.wko_mst_wo_no = @wo_no
+join wkr_mst w on w.wkr_mst_wr_no  = d.wko_det_wr_no
+
+if @wr_date >  @cmpl_date
+begin 
+RAISERROR('Completion date should be greater than work request date',16,1);RETURN
+END
+if @wr_date >  @response_date
+begin 
+RAISERROR('Response date should be greater than work request date',16,1);RETURN
+END
+if @wr_date >  @wo_ack_date
+begin 
+RAISERROR('Acknowledge date should be greater than work request date',16,1);RETURN
+END
+if @wo_ack_date > @response_date
+BEGIN
+SELECT @response_date '@response_date',@wo_ack_date '@wo_ack_date'
+RAISERROR('Response date should be greater than Acknowledge date',16,1);RETURN
+END
+if (@response_date > @cmpl_date) or (@wo_ack_date > @cmpl_date)
+BEGIN
+SELECT @response_date '@response_date',@cmpl_date '@cmpl_date',@wo_ack_date '@wo_ack_date'
+RAISERROR('Completion date should be greater than Response date and Acknowledge date ',16,1);RETURN
+END
+
 
 update d 
 set d.wko_det_cmpl_date = isnull(@cmpl_date,d.wko_det_cmpl_date)
@@ -14,6 +47,5 @@ JOIN	wko_det d (NOLOCK)
 on m.RowID =d.mst_RowID
 and m.wko_mst_wo_no = @wo_no
 
-
-set nocount OFF
+ 
 end
